3bf30678e11f2c0fd2269e45c6de054b6ee6894f
Xian Ming Lee <xlee@guidewire.com>
2022-06-13 17:53:12 +0800

    Changed the server.js configurations

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/paymentology-angular-xianming'));
+app.use(express.static('./dist/paymentology-frontend'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/paymentology-angular-xianming/'})
+    res.sendFile('index.html', {root: 'dist/paymentology-frontend/'})
 });
 
 app.listen(process.env.PORT || 8084);
