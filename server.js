83e0b1efe5fd99914b91caf95efa0c64bf90f817
Xian Ming Lee <xlee@guidewire.com>
2022-06-13 17:49:21 +0800

    Changed the server.js configurations

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/paymentology-xianming-poc'));
+app.use(express.static('./dist/paymentology-angular-xianming'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'})
+    res.sendFile('index.html', {root: 'dist/paymentology-angular-xianming/'})
 });
 
 app.listen(process.env.PORT || 8084);
